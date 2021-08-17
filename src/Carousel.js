294c877e72bf2afb63b5aabbdb9705e43330ddca
louisOhlow <louisohlow@gmail.com>
2021-08-17 12:50:16 +0200

    added image carousel

--- src/Carousel.js
+++ src/Carousel.js
@@ -2,7 +2,15 @@ import "./Carousel.css";
 import React from "react";
 import placeHolder from "./res/thinkMoto-slide-1.png";
 import ImageCard from "./ImageCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Virtual, Pagination, EffectCoverflow } from "swiper";
 import { connect } from "react-redux";
+
+import "swiper/swiper.min.css";
+import "swiper/components/effect-coverflow/effect-coverflow.min.css";
+import "swiper/components/pagination/pagination.min.css";
+
+SwiperCore.use([Virtual, Pagination, EffectCoverflow]);
 
 class Carousel extends React.Component {
   constructor(props) {
@@ -31,18 +39,55 @@ class Carousel extends React.Component {
     const { imageFile } = this.state;
     return (
       <div className="carousel-container">
-        <div className="carousel">
-          <ImageCard imageUrl={imageFile} filterType="none" index={0} />
-          <ImageCard imageUrl={imageFile} filterType="noRed" index={1} />
-          <ImageCard imageUrl={imageFile} filterType="noGreen" index={2} />
-          <ImageCard imageUrl={imageFile} filterType="blackWhite" index={3} />
-          <canvas
-            id="canvass"
-            ref={(canvasA) => (this.canvasA = canvasA)}
-            width={1024}
-            height={1024}
-          />
-        </div>
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={"auto"}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          loop
+          pagination={true}
+          className="swiper-container"
+        >
+          <SwiperSlide id="slider1" className="swiper-slide">
+            <ImageCard
+              className="swiper-slide-image"
+              imageUrl={imageFile}
+              filterType="none"
+              index={0}
+            />
+          </SwiperSlide>
+          <SwiperSlide id="slider2" className="swiper-slide">
+            <ImageCard
+              className="swiper-slide-image"
+              imageUrl={imageFile}
+              filterType="noRed"
+              index={1}
+            />
+          </SwiperSlide>
+          <SwiperSlide id="slider3" className="swiper-slide">
+            <ImageCard
+              className="swiper-slide-image"
+              imageUrl={imageFile}
+              filterType="noGreen"
+              index={2}
+            />
+          </SwiperSlide>
+          <SwiperSlide id="slider4" className="swiper-slide">
+            <ImageCard
+              className="swiper-slide-image"
+              imageUrl={imageFile}
+              filterType="blackWhite"
+              index={3}
+            />
+          </SwiperSlide>
+        </Swiper>
         <div className="buttons">
           <div className="uploadButton">
             <input type="file" onChange={this.handleUpload} />
